cfecba957911c7b89506949a652faf38c6d64abb
Brad Lindgren <bplindgren91@gmail.com>
2016-11-01 11:33:15 -0500

    first commit

---
+++ src/main/java/com/bradley/CuttlefernsApplication.java
@@ -0,0 +1,12 @@
+package com.bradley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuttlefernsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuttlefernsApplication.class, args);
+	}
+}
